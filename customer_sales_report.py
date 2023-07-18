38fc70af439d0dff4584c2100266967d677c7c0b
Sanjay Krishnamoorthy <sanjay_krishnamoort1@baylor.edu>
2023-07-17 21:36:59 -0400

    added and changed salesreport

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -9,7 +9,7 @@ with open('sales.csv', 'r') as file:
     reader = csv.reader(file1)
     next(reader)
     print('Customer ID, Total')
-    
+
     current_ID = 250
     current_sales = 0
     total_sales = 0
@@ -19,7 +19,7 @@ with open('sales.csv', 'r') as file:
             total_sales += current_sales
         else:
             file2.write(str(current_ID))
-            file2.write(' ')
+            file2.write(', ')
             file2.write(str(round(total_sales, 2)))
             file2.write(' ')
             file2.write('\n')
@@ -29,7 +29,7 @@ with open('sales.csv', 'r') as file:
             current_sales = total_sales + float(row[3]) + float(row[4]) + float(row[5])
             total_sales += current_sales
     file2.write(str(current_ID))
-    file2.write(' ')
+    file2.write(', ')
     file2.write(str(total_sales))
     file2.write(' ')
     file2.write('\n')
